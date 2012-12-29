779fb8cc9698d49d0d1f1f8fd180f58b0303a23f
hbt <hassenbentanfous@gmail.com>
2012-12-29 00:55:05 -0500

    -#29 adds r.js + build.js -- working optimization script but code require changes to detect nested dependencies

---
+++ config/requirejs/build.js
@@ -0,0 +1,9 @@
+({
+  name:                   'main',
+  optimize:               "none",
+  findNestedDependencies: true,
+  mainConfigFile:         '../../app/web/assets/js/main.js',
+  appDir:                 '../../app/web',
+  baseUrl:                'assets/js',
+  'dir':                  '../../app/web-built',
+})
