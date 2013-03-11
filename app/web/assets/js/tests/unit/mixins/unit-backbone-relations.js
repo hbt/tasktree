5647e0412260a7812ac09e06b027c5d25a281133
hbt <hassenbentanfous@gmail.com>
2013-03-10 20:42:09 -0400

    vendor relational | adds tests for relations (many to many, one to many)

--- app/web/assets/js/tests/unit/mixins/unit-backbone-relations.js
+++ app/web/assets/js/tests/unit/mixins/unit-backbone-relations.js
@@ -13,54 +13,112 @@ define(['utils/tests/helpers'], function(TestUtils)
         child = App.models.Task.prototype.global.create({content: 'child 1'})
       })
 
+      // TODO(hbt) Refactor (low): split into smaller tests like the many to many -- too many assertions
       it('should add', function()
       {
         // add child
         parent.get('children').add(child)
         parent.save()
 
+        // verify relations are updated
         assert.is(parent.get('children').pluck('id')[0], child.get('id'))
         assert.is(child.get('parent').get('id'), parent.get('id'))
 
-        child2 = App.models.Task.prototype.global.create({content: 'child 2'})
+        // same reference
+        assert.is(child.get('parent'), parent)
+
+        // add another child
+        var child2 = App.models.Task.prototype.global.create({content: 'child 2'})
         parent.get('children').add(child2)
         parent.save()
 
+        // verify nothing is overwritten
         assert.is(parent.get('children').pluck('id')[1], child2.get('id'))
         assert.is(child2.get('parent').get('id'), parent.get('id'))
         assert.is(child.get('parent').get('id'), parent.get('id'))
+      })
 
-//        assert.is(parent.get('children').pluck('id'))
 
-//        var pid = parent.get('id'), cid = child.get('id')
-//        parent = null, child = null
-//        console.log(parent, child)
-//
-//        TestUtils.reset()
-        // retrieve
+      it('should remove', function()
+      {
+        assert.is(parent.get('children').length, 2)
+        var first = parent.get('children').first()
+        assert.is(first.get('parent').get('id'), parent.get('id'))
+
+        // delete
+        parent.get('children').remove(first)
+        parent.save()
 
         // verify
-
-        // verify reference
-//        assert.is(parent.get('children'))
-      })
-
-
-      xit('should remove', function()
-      {
-
-      })
-
-
-      xit('', function()
-      {
-
+        assert.is(parent.get('children').length, 1)
+        assert.is(first.get('parent'), null)
       })
     });
 
     describe('many to many', function()
     {
 
+      var task1, task2, tag1, tag2
+      before(function()
+      {
+        TestUtils.reset()
+        task1 = App.models.Task.prototype.global.create({content: 'task 1'})
+        task2 = App.models.Task.prototype.global.create({content: 'task 2'})
+
+        tag1 = App.models.Tag.prototype.global.create({content: 'tag 1'})
+        tag2 = App.models.Tag.prototype.global.create({content: 'tag 2'})
+      })
+
+      describe('adding', function()
+      {
+        it('should add related', function()
+        {
+          task1.get('tags').add([tag1, tag2])
+          task1.save()
+
+          assert.is(task1.get('tags').length, 2)
+          assert.is(tag1.get('tasks').length, 1)
+          assert.is(tag2.get('tasks').length, 1)
+        })
+
+        it('should share the same reference', function()
+        {
+          // same reference
+          assert.is(tag1.get('tasks').first(), task1)
+        })
+
+        it('should update and not overwrite', function()
+        {
+          // verify overwrite
+          task2.get('tags').add([tag1, tag2])
+          task2.save()
+
+
+          assert.is(task1.get('tags').length, 2)
+          assert.is(task2.get('tags').length, 2)
+          assert.is(tag1.get('tasks').length, 2)
+          assert.is(tag2.get('tasks').length, 2)
+
+        })
+      });
+
+
+      describe('removing', function()
+      {
+        it('should remove', function()
+        {
+          var first = task1.get('tags').first()
+          task1.get('tags').remove(first)
+          task1.save()
+
+
+          assert.is(task1.get('tags').length, 1)
+          assert.is(task2.get('tags').length, 2)
+          assert.is(tag1.get('tasks').length, 1)
+          assert.is(tag2.get('tasks').length, 2)
+
+        })
+      });
     });
 
 
